c695b55c6af813a32912f2e9a4840d3e64db1462
kei <kskkwn@gmail.com>
2016-04-22 00:06:05 +0900

    restore all windows and buffers when restart emacs

--- init.el
+++ init.el
@@ -39,3 +39,20 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 ;;~/.emacs.d/inits/ 以下を読み込み
 (init-loader-load "~/.emacs.d/inits")
+
+;;再起動時に色々復元
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(desktop-save-mode t)
+ '(helm-mini-default-sources
+   (quote
+    (helm-source-buffers-list helm-source-recentf helm-source-files-in-current-dir helm-source-emacs-commands-history helm-source-emacs-commands))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
