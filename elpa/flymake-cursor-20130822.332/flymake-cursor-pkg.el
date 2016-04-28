d5f06bc3ba8f5631795b94229e32d0fb0231e02a
kei <kskkwn@gmail.com>
2016-04-28 22:48:17 +0900

    add flymake cursor

---
+++ elpa/flymake-cursor-20130822.332/flymake-cursor-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flymake-cursor" "20130822.332" "displays flymake error msg in minibuffer after delay" 'nil :url "http://www.emacswiki.org/emacs/flymake-cursor.el" :keywords '("languages" "mode" "flymake"))
