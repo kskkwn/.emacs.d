cad594de21b4bc471d3fc57c93e2496313743af2
kei <kskkwn@gmail.com>
2017-02-15 06:55:09 +0900

    block align nil in magic-latex-buffer

--- inits/11-latex-mode.el
+++ inits/11-latex-mode.el
@@ -2,6 +2,7 @@
  'LaTeX-mode-hook
  '(lambda ()
     (setq reftex-default-bibliography '("~/reference.bib"))
+    (setq magic-latex-enable-block-align nil)
     (magic-latex-buffer)
     (reftex-mode t)
     (smartparens-mode)
