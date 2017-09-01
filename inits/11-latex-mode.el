0cf378bd5fb33d5b6935980354f0cfcaa7d6e6fa
ksknw <kskkwn@gmail.com>
2017-09-01 11:03:22 +0900

    Update 11-latex-mode.el
    disable magic latex buffer

--- inits/11-latex-mode.el
+++ inits/11-latex-mode.el
@@ -2,8 +2,8 @@
  'LaTeX-mode-hook
  '(lambda ()
     (setq reftex-default-bibliography '("~/reference.bib"))
-    (setq magic-latex-enable-block-align nil)
-    (magic-latex-buffer)
+    ;;(setq magic-latex-enable-block-align nil)
+    ;;(magic-latex-buffer)
     (reftex-mode t)
     (smartparens-mode)
     (LaTeX-math-mode t)
