cd3440af9bc3711b65be01614a5eb103d07c8279
kskkwn <kskkwn@gmail.com>
2018-01-29 22:23:56 +0900

    add align regexp repeated

--- inits/01-common.el
+++ inits/01-common.el
@@ -115,3 +115,23 @@
 
 (require 'helm-perspeen)
 
+;; http://kiyotakagoto.hatenablog.com/entry/2013/06/04/003605
+(defun align-regexp-repeated (start stop regexp)
+  "Like align-regexp, but repeated for multiple columns. See
+http://www.emacswiki.org/emacs/AlignCommands"
+  (interactive "r\nsAlign regexp: ")
+  (let ((spacing 1)
+        (old-buffer-size (buffer-size)))
+    ;; If our align regexp is just spaces, then we don't need any
+    ;; extra spacing.
+    (when (string-match regexp " ")
+      (setq spacing 0))
+    (align-regexp start stop
+                  ;; add space at beginning of regexp
+                  (concat "\\([[:space:]]*\\)" regexp)
+                  1 spacing t)
+    ;; modify stop because align-regexp will add/remove characters
+    (align-regexp start (+ stop (- (buffer-size) old-buffer-size))
+                  ;; add space at end of regexp
+                  (concat regexp "\\([[:space:]]*\\)")
+                  1 spacing t)))
